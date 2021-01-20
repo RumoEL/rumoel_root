d42f958aab8e0a39b5f6eed09dd01be9d59d4193
RumoEL <tbr4dDEqRrz25WwRQstR5B1S1RVQ@gmail.com>
2021-01-20 19:25:36 +0000

    init

---
+++ rumoel_hub/src/main/java/com/github/rumoel/hub/init/RumoelHubApplication.java
@@ -0,0 +1,13 @@
+package com.github.rumoel.hub.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumoelHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumoelHubApplication.class, args);
+	}
+
+}
---
+++ rumoel_hub/src/test/java/com/github/rumoel/hub/init/RumoelHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rumoel.hub.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumoelHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
